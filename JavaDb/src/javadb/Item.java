f1b938d6ed8cdd508177793be2f01cc08c6d5663
Yusuf <ahmadyusuf@uinsa.ac.id>
2025-11-11 14:30:22 +0700

    inisialisasi project

---
+++ JavaDb/src/javadb/Item.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javadb;
+
+/**
+ *
+ * @author Ahmad Yusuf
+ */
+public class Item {
+    public int id;
+    public String name;
+    public int quantity;
+
+    public Item() {}
+
+    public Item(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+    
+}
